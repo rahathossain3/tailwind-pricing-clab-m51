1ce0230f9cfeb705b145db39b653d656b03ddf52
rahathossain3 <rahathossain210@gmail.com>
2022-03-28 13:13:12 +0600

    h

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     ]
     return (
         <nav>
-            <ul>
+            <ul className='md:flex justify-center'>
                 {
                     routes.map(route => <Link
                         key={route.id}
